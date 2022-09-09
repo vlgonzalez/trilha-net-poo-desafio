f62a151d11f4d38e7f788f9fd23e423427965231
VINICIUS LARANGEIRA GONZALEZ <vinicius.vlg@hotmail.com>
2022-09-09 17:37:39 -0300

    desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,17 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo;
+        private string IMEI;
+        private int Memoria;
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int meoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = meoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
-
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Celular Nokia");
+Smartphone nokia = new Nokia ("11967813116", "Nokia Indestrutivel 1", "7778963288231", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Youtube");
+
+Console.WriteLine("/n");
+
+Console.WriteLine($"Celular Iphone");
+Smartphone iphone = new Iphone ("11938537556", "Iphone Sem bateria", "3658963288928", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("iTunes");
